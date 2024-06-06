855aedfcbc50cde8ac17caef368a5d705c8e2fa9
xianghuan <xianghuan@cmss.chinamobile.com>
2024-06-06 17:57:08 +0800

    vue2语法改为vue3语法，引入element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  server: {
+    host: '0.0.0.0', // 这个用于启动
+    port: '8092', // 指定启动端口
+    open: true //启动后是否自动打开浏览器
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
